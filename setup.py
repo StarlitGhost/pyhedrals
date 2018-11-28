dac771d676b66e969dcd2e3146fe2be061f42d93
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2018-11-28 18:13:40 +0000

    bump the version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "pyhedrals",
-    version = "0.0.3",
+    version = "0.1.0",
     author = "StarlitGhost",
     author_email = "starlitwraith@gmail.com",
     description = "A library for evaluating tabletop dice roll expressions",
